49d15847382e55e38f2e1c2d04a101fae2c47a94
lingshin <yokaringx@outlook.com>
2025-06-17 18:39:28 +0800

    doc(nvim.term): add useful comment
    souvenir

--- lua/plugins/ui/term.lua
+++ lua/plugins/ui/term.lua
@@ -1,15 +1,42 @@
 return {
   "akinsho/toggleterm.nvim",
+  enable = false,
+  lazy = false,
+  -- I can't copy this to my system clipboard maybe because it is headless
+  -- I think it is because it's neovide
+  -- i cannot copy/paste in neovide with cmd-c / cmd-v
+  -- But you can still yy/dd/p right? yes. dd to your system clipboard?
+  -- maybe with "+yy can do this
+  -- in my neovide is automatic sync
+  -- look "+"
+  -- yep.
+  -- idk how , it sometimes workso on my pc
+  -- idk too
+  -- how dare you to v-select before delete
+  -- ei? is that dangrous?
+  -- you should d9k
+  -- sorry it's so dimming i cannot see the line number
+  -- oh god. my clipboard borken? or neovide broken?
+  -- alright
+  -- i wanna know more aboud terminal in neovide. plz show me QAQ
+  -- <C-\> to toggle
   opts = {
     open_mapping = [[<C-\>]],
     hide_numbers = true,
     start_in_insert = true,
-    direction = "float",
+    direction = "float", --<-- this ok
+    -- let me do it for my toggleterm,
+    -- I'll copy'em for you
+    -- :)
     float_opts = {
       border = "rounded",
-      width = 130,
+      width = function()
+        local min = math.ceil(vim.o.columns * 0.8)
+        return min > 130 and 130 or min
+      end,
       title_pos = "center",
     },
+    -- it this ?
     winbar = {
       enable = true,
       name_formatter = function(term)
