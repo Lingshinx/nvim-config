22b05d46ce62543d162cc78c2e927fffe4774b4c
lingshin <yokaringx@outlook.com>
2025-06-17 17:40:31 +0800

    fix(nvim.flatten): make the config struct correct
    我一直以来是个笨蛋

--- lua/plugins/utils/flatten.lua
+++ lua/plugins/utils/flatten.lua
@@ -1,11 +1,10 @@
 return {
   "willothy/flatten.nvim",
-  config = true,
-  opts = {},
   lazy = false,
   priority = 1001,
-  window = {
-    open = "alternate",
+  opts = {
+    window = {
+      open = "alternate",
+    },
   },
-  open = "current",
 }
