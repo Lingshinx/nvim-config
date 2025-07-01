ae14e1f3a2d2ab26797bd79727099cb8aa897a2b
lingshin <yokaringx@outlook.com>
2025-07-01 16:10:04 +0800

    fix(nvim.autocmd): removed auto git stage after leaving
    果然不是很好的功能

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,16 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Git stage on VimLeave event
-vim.api.nvim_create_autocmd("VimLeave", {
-  callback = function()
-    local git_path = Snacks.git.get_root()
-    if git_path then
-      vim.system({ "git", "stage", "." })
-    end
-  end,
-})
-
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   callback = function()
     if vim.bo.buftype ~= "" or vim.o.filetype == "oil" then
