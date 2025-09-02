cf0418bf8d92782d2dd77ead7317505bf99673ed
lingshin <yokaringx@outlook.com>
2025-09-02 22:08:03 +0800

    format(nvim.plugin.mason)

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,34 +1,32 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		},
-		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-	},
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+  },
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		lazy = true,
-		opts = function()
-			return {
-				ensure_installed = require("config.lsp").mason,
-			}
-		end,
-	},
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = true,
+    opts = {
+      ensure_installed = require("config.lsp").mason,
+    },
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		event = "LazyFile",
-		dependencies = {
-			"mason.nvim",
-			"mason-org/mason-lspconfig.nvim",
-		},
-	},
+  {
+    "neovim/nvim-lspconfig",
+    event = "LazyFile",
+    dependencies = {
+      "mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+  },
 }
