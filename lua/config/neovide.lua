cc957170981c36e2301f92df766fdfa4050a0a42
lingshin <yokaringx@outlook.com>
2025-09-04 09:07:25 +0800

    format(nvim): just format

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,8 +1,5 @@
-if not vim.g.neovide then
-	return
-end
+if not vim.g.neovide then return end
 
-vim.g.neovide_input_time = true
 vim.g.neovide_padding_top = 10
 vim.g.neovide_padding_right = 0
 
@@ -16,17 +13,16 @@ vim.g.neovide_cursor_unfocuesd_outline_width = 0
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_cursor_smooth_blink = true
 
+vim.g.neovide_input_ime = false
 vim.api.nvim_create_autocmd({
-	"InsertEnter",
-	"InsertLeave",
-	"TermEnter",
-	"TermLeave",
-	"CmdlineEnter",
-	"CmdlineLeave",
+  "InsertEnter",
+  "InsertLeave",
+  "TermEnter",
+  "TermLeave",
+  "CmdlineEnter",
+  "CmdlineLeave",
 }, {
-	group = vim.api.nvim_create_augroup("ime_input", { clear = true }),
-	pattern = "*",
-	callback = function(args)
-		vim.g.neovide_input_ime = not not args.event:match("Enter$")
-	end,
+  group = vim.api.nvim_create_augroup("ime_input", { clear = true }),
+  pattern = "*",
+  callback = function(args) vim.g.neovide_input_ime = not not args.event:match "Enter$" end,
 })
