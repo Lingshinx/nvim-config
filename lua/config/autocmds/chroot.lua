01b01c4d270d6c717ed6a192cf97d334f4bd9d7d
lingshin <yokaringx@outlook.com>
2025-09-04 16:39:01 +0800

    refactor(nvim.autocmds): collect autocmds into a single file

--- lua/config/autocmds/chroot.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	desc = "Auto change dir to root",
-	callback = function()
-		if vim.bo.buftype ~= "" then
-			return
-		end
-		vim.fn.chdir(require("config.utils.root").get())
-	end,
-})
--- lua/config/autocmds/format.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		if vim.g.autoformat and vim.b.autoformat ~= false then
-			require("conform").format({ bufnr = args.buf })
-		end
-	end,
-})
