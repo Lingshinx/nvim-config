78c75d042f8c39bfb6d7134c11991a8204f2c066
lingshin <yokaringx@outlook.com>
2025-09-07 19:18:32 +0800

    format(nvim.plugin.lualine)

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,99 +1,83 @@
-local icons = require("config.icons")
+local icons = require "config.icons"
 
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	dependencies = { "nvim-tree/nvim-web-devicons", "folke/trouble.nvim" },
-	opts = function()
-		return {
-			options = {
-				theme = "auto",
-				globalstatus = true,
-				disabled_filetypes = { statusline = { "snacks_dashboard" } },
-			},
-			sections = {
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						"diagnostics",
-						symbols = {
-							error = icons.diagnostics.Error,
-							warn = icons.diagnostics.Warn,
-							info = icons.diagnostics.Info,
-							hint = icons.diagnostics.Hint,
-						},
-					},
-					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-					{ require("config.utils.plugin.lualine").pretty_path },
-				},
-				lualine_x = {
-					{
-						function()
-							return require("noice").api.status.command.get()
-						end,
-						cond = function()
-							return package.loaded["noice"] and require("noice").api.status.command.has()
-						end,
-						color = function()
-							return { fg = Snacks.util.color("Statement") }
-						end,
-					},
-					{
-						function()
-							return require("noice").api.status.mode.get()
-						end,
-						cond = function()
-							return package.loaded["noice"] and require("noice").api.status.mode.has()
-						end,
-						color = function()
-							return { fg = Snacks.util.color("Constant") }
-						end,
-					},
-					{
-						"diff",
-						symbols = {
-							added = icons.git.added,
-							modified = icons.git.modified,
-							removed = icons.git.removed,
-						},
-						source = function()
-							local gitsigns = vim.b.gitsigns_status_dict
-							if gitsigns then
-								return {
-									added = gitsigns.added,
-									modified = gitsigns.changed,
-									removed = gitsigns.removed,
-								}
-							end
-						end,
-					},
-				},
-				lualine_y = {
-					{ "location", padding = { left = 0, right = 1 } },
-				},
-				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
-				},
-			},
-		}
-	end,
-	config = function(_, opts)
-		local symbols = require("trouble").statusline({
-			mode = "symbols",
-			groups = {},
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name:Normal}",
-			hl_group = "lualine_c_normal",
-		})
-		table.insert(opts.sections.lualine_c, {
-			symbols and symbols.get,
-			cond = function()
-				return vim.b.trouble_lualine ~= false and symbols.has()
-			end,
-		})
-		require("lualine").setup(opts)
-	end,
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons", "folke/trouble.nvim" },
+  opts = function()
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "snacks_dashboard" } },
+      },
+      sections = {
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { require("config.utils.plugin.lualine").pretty_path },
+        },
+        lualine_x = {
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = function() return { fg = Snacks.util.color "Statement" } end,
+          },
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return { fg = Snacks.util.color "Constant" } end,
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_y = {
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function() return " " .. os.date "%R" end,
+        },
+      },
+    }
+  end,
+  config = function(_, opts)
+    local symbols = require("trouble").statusline {
+      mode = "symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      hl_group = "lualine_c_normal",
+    }
+    table.insert(opts.sections.lualine_c, {
+      symbols and symbols.get,
+      cond = function() return vim.b.trouble_lualine ~= false and symbols.has() end,
+    })
+    require("lualine").setup(opts)
+  end,
 }
