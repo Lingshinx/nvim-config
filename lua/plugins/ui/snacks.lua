c135abc531213401965e95b64174ac2ec95ad4c7
lingshin <yokaringx@outlook.com>
2025-10-03 17:57:25 +0800

    format(nvim.plugins.snacks)

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -1,14 +1,10 @@
 local function pick(cmd)
-	return function()
-		Snacks.dashboard.pick(cmd)
-	end
+  return function() Snacks.dashboard.pick(cmd) end
 end
 local function pickfiles(cwd)
-	return function()
-		Snacks.dashboard.pick("files", { cwd = cwd })
-	end
+  return function() Snacks.dashboard.pick("files", { cwd = cwd }) end
 end
-local config_dir = vim.fn.stdpath("config")
+local config_dir = vim.fn.stdpath "config"
 local dot_dir = vim.env.XDG_CONFIG_HOME or "~/.config"
 local logo = [[
      ██╗     ██╗███╗   ██╗ ██████╗ ███████╗██╗  ██╗██╗███╗   ██╗██╗     
@@ -20,17 +16,17 @@ local logo = [[
                                 -------------聆噺讨厌写代码             
 ]]
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = function()
-		---@type snacks.Config
-		return {
-			scroll = { enabled = true },
-			statuscolumn = { enabled = true },
-			dashboard = {
-				preset = {
-					header = logo,
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = function()
+    ---@type snacks.Config
+    return {
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      dashboard = {
+        preset = {
+          header = logo,
           -- stylua: ignore
 					---@type snacks.dashboard.Item[]
 					keys = {
@@ -43,8 +39,8 @@ return {
 						{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
 						{ icon = " ", key = "q", desc = "Quit", action = ":qa" }
 					},
-				},
-			},
-		}
-	end,
+        },
+      },
+    }
+  end,
 }
