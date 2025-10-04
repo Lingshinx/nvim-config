a99aed12d4ee2c44bfe2c4a1fc544832c6a37c94
lingshin <yokaringx@outlook.com>
2025-10-04 15:09:26 +0800

    add(nvim.utils.fn.capitalize)

--- lua/config/utils/fn.lua
+++ lua/config/utils/fn.lua
@@ -1,17 +1,17 @@
 local M = {}
 M = {
-	---@param f fun(x):integer
-	---@return fun(a, b):boolean
-	comparing = function(f)
-		return function(a, b)
-			return f(a) < f(b)
-		end
-	end,
+  ---@param f fun(x):integer
+  ---@return fun(a, b):boolean
+  comparing = function(f)
+    return function(a, b) return f(a) < f(b) end
+  end,
 
-	---@param str string
-	---@return integer
-	length = function(str)
-		return str and #str or 0
-	end,
+  ---@param str string
+  ---@return integer
+  length = function(str) return str and #str or 0 end,
+
+  ---@param str string
+  ---@return string
+  capitalize = function(str) return str:sub(1, 1):upper() .. str:sub(2) end,
 }
 return M
