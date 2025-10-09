76b04f3c0900439c0f8d792195e9acb67423dd64
lingshin <yokaringx@outlook.com>
2025-10-10 00:25:03 +0800

    feat(nvim.plugins.picker): a little tweak

--- lua/plugins/component/picker.lua
+++ lua/plugins/component/picker.lua
@@ -15,6 +15,32 @@ return {
           unselected = false,
         },
       },
+      layouts = {
+        vscode = {
+          layout = {
+            row = 2,
+          },
+        },
+        sidebar = {
+          preview = "main",
+          layout = {
+            backdrop = false,
+            width = 40,
+            min_width = 40,
+            height = 0,
+            position = "left",
+            box = "vertical",
+            {
+              win = "input",
+              height = 1,
+              border = "hpad",
+              title_pos = "center",
+            },
+            { win = "list", border = "none" },
+            { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+          },
+        },
+      },
       win = {
         input = {
           keys = {
