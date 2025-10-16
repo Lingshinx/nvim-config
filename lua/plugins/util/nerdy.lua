282849d3b562cd9f96b3f7e04a74acb2978d4abb
lingshin <yokaringx@outlook.com>
2025-10-16 20:41:19 +0800

    remove(plugins.nerdy): I can use snacks.picker.icons as an alternatives

--- lua/plugins/util/nerdy.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "2kabhishek/nerdy.nvim",
-  cmd = "Nerdy",
-  keys = { { "<leader>sN", "<cmd>Nerdy<CR>", desc = "Nerd Icons" } },
-  opts = {
-    add_default_keybindings = false,
-    use_new_command = true,
-  },
-}
