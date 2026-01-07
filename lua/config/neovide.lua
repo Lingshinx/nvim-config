4e611dedcaedfa32d7fe4712eeaa6aa3fc43d720
lingshin <yokaringx@outlook.com>
2026-01-08 00:52:20 +0800

    perf(neovide.options): remove some nonsense sentence

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -5,11 +5,8 @@ vim.g.neovide_padding_right = 0
 
 vim.g.neovide_opacity = 0.95
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_input_ime = true
-vim.g.neovide_underline_stroke_scale = 1.0
 
 vim.g.neovide_cursor_unfocused_outline_width = 0
-vim.g.neovide_cursor_unfocuesd_outline_width = 0
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_cursor_smooth_blink = true
 
