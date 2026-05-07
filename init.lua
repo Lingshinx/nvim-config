f331e6420586011a0ab14c7e2e43909ecff13b97
lingshin <yokaringx@outlook.com>
2026-05-07 15:31:07 +0800

    refactor(utils.lang): more extentable

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 -- require "utils.profiler"
 require "config.options"
 local wait = require "load.packs"
+local langs = require "load.langs"
 require "load.colorscheme"
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "DeferredUIEnter",
   callback = function()
+    langs:load()
     require "load.keymap"
     require "config.autocmds"
     require "config.neovide"
--- lua/utils/language/langs.lua
+++ lua/utils/language/langs.lua
@@ -1,107 +1,151 @@
-local Lang = require "utils.language.lang"
-local append = require("utils.list").append
-local mapfold = require("utils.list").mapfold
+---@class utils.language.TypedExtractor
+---@field string? boolean | fun(property:string, name:string, lang: table):any
+---@field boolean? boolean | fun(property:boolean, name:string, lang: table):any
+---@field t? boolean | fun(name: string, lang: table):any
+---@field f? boolean | fun(name: string, lang: table):any
+---@field table? boolean | fun(property: table, name: string, lang: table):any
+---@field pair? fun(key: any, value: any, name: string, lang: table):any -- because pair == true is equal to table = true
+---@field spair? fun(key: string, value: any, name: string, lang: table):any
+---@field ipair? fun(index: number, value: any, name: string, lang: table):any
 
-local properties = {
-  formatters = function(result, config) result[config.name] = config.formatter end,
-  plugins = function(result, config) append(result, config.plugins) end,
-  treesitter = function(result, config) vim.list_extend(result, config.treesitter or {}) end,
-  lsp = function(result, config) append(result, config.lsp) end,
-  mason = function(result, config)
-    vim.list_extend(result, config:get_lspnames() or {})
-    vim.list_extend(result, config.formatter or {})
-    vim.list_extend(result, config.pkgs or {})
-  end,
-}
+---@generic T,U
+---@class utils.language.Collector
+---@field extract utils.language.TypedExtractor | boolean | fun(property:any, name: string, lang: table):T?
+---@field load? fun(lang: utils.language.Langs)
+---@field each? fun(property:any, name: string ,lang: table)
 
----@alias config.language.Collect fun(lang:config.language.Langs, callback: fun(result:table, config:config.language.Langs)):table
-local lang_functions = {
-  ---@param langs config.language.Langs
-  ---@param config config.language.Config
-  ---@param name string
-  append = function(langs, name, config) langs.get[name] = Lang.new(name, config) end,
+---@alias utils.language.Collectors table<string, utils.language.Collector>
 
-  config_lsp = function(langs)
-    for _, config in pairs(langs.get) do
-      config:config_lsp()
+---@class utils.language.Langs
+---@field collectors utils.language.Collectors
+---@field data table<string,table<string,any>>
+---@field new fun(opts: utils.language.Collectors)
+---@field solve fun(self:self, spec:table)
+---@field extract fun(self:self, name:string, spec:table)
+---@field fold fun(self:self, property: string, fold: fun(acc, cur):any, init:any?):any
+---@field load fun(self:self)
+---@field [string] any
+
+local Langs = {}
+Langs.__index = Langs ---@diagnostic disable-line
+
+---@param collectors utils.language.Collector?
+---@return utils.language.Langs
+function Langs.new(collectors)
+  return setmetatable(
+    { collectors = vim.tbl_deep_extend("force", require "utils.language.collectors", collectors or {}), data = {} },
+    Langs
+  )
+end
+
+---@param extractor boolean|fun(property, name:string, lang: table)
+---@param property any
+---@param lang table
+---@return any?
+local function extract(extractor, property, name, lang)
+  if type(extractor) == "boolean" and extractor then return property end
+  if type(extractor) == "function" then return extractor(property, name, lang) end
+end
+
+---@param extractor utils.language.TypedExtractor
+---@param property any
+---@param name string
+---@param lang table
+local function extract_by_table(extractor, property, name, lang)
+  local property_type = type(property)
+  if property_type == "string" then
+    return extract(extractor.string, property, name, lang)
+  elseif property_type == "boolean" then
+    if extractor.t and property then return type(extractor.t) == "function" and extractor.t(name, lang) or property end
+    if extractor.f and not property then
+      return type(extractor.t) == "function" and extractor.f(name, lang) or property
     end
-  end,
-
-  config_options = function(langs)
-    for _, config in pairs(langs.get) do
-      config:config_options()
+    return extract(extractor.boolean, property, name, lang)
+  elseif property_type == "table" then
+    if extractor.pair then
+      return vim.iter(pairs(property)):map(function(k, v) return extractor.pair(k, v, name, lang) end):totable()
     end
-  end,
-
-  config_mason = function(langs)
-    for _, pkg in ipairs(langs.mason) do
-      require("utils.plugin.mason").install(pkg)
+    if extractor.spair and extractor.ipair then
+      return vim
+        .iter(pairs(property))
+        :map(
+          function(k, v)
+            return type(k) == "number" and extractor.ipair(k, v, name, lang) or extractor.spair(k, v, name, lang)
+          end
+        )
+        :totable()
     end
-  end,
+    if extractor.spair then
+      return vim
+        .iter(pairs(property))
+        :map(function(k, v) return type(k) == "string" and extractor.spair(k, v, name, lang) or nil end)
+        :totable()
+    end
+    if extractor.ipair then
+      return vim
+        .iter(pairs(property))
+        :map(function(k, v) return type(k) == "number" and extractor.ipair(k, v, name, lang) or nil end)
+        :totable()
+    end
+    return extract(extractor.table, property, name, lang)
+  end
+end
 
-  config_treesitter = function(langs)
-    require("utils.plugin.treesitter").ensure_installed(langs.treesitter)
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = langs.treesitter,
-      callback = function()
-        vim.treesitter.start()
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.wo.foldmethod = "expr"
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
+---@param name string
+---@param self utils.language.Langs
+---@param spec table
+function Langs:extract(name, spec)
+  local extracted = {}
+  for property, collector in pairs(self.collectors) do
+    local extractor = collector.extract
+    if type(extractor) == "boolean" and extractor then
+      extracted[property] = spec[property]
+    elseif type(extractor) == "function" then
+      extracted[property] = extractor(spec[property], name, spec)
+    elseif type(extractor) == "table" then
+      extracted[property] = extract_by_table(extractor, spec[property], name, spec)
+    end
+  end
+  return extracted
+end
 
-  config_formatter = function(langs)
-    local conform = require "conform"
-    conform.formatters_by_ft = vim.tbl_extend("force", conform.formatters_by_ft, langs.formatters)
-  end,
+---@param self utils.language.Langs
+---@param spec table
+function Langs:solve(spec)
+  local mt = { __index = spec }
+  for _, lang in ipairs(spec) do
+    if type(lang) == "string" then
+      self.data[lang] = self:extract(lang, spec)
+    elseif type(lang) == "table" then
+      self:solve(setmetatable(lang, mt))
+    end
+  end
+end
 
-  config = function(langs, opts)
-    langs.ok = true
-    if opts then
-      for _, v in ipairs(opts) do
-        langs["config_" .. v](langs)
+---@param property string
+---@param fold fun(acc, cur)
+---@param init any
+function Langs:fold(property, fold, init)
+  local acc = init or {}
+  for _, lang in pairs(self.data) do
+    local cur = lang[property]
+    if cur then acc = fold(acc, cur) end
+  end
+  return acc
+end
+
+---@param self utils.language.Langs
+function Langs:load()
+  for property, collector in pairs(self.collectors) do
+    if collector.load then collector.load(self) end
+    if collector.each then
+      for name, lang in pairs(self.data) do
+        if(lang[property]) then
+          collector.each(lang[property], name, lang)
+        end
       end
-      return
     end
-    langs:config_options()
-    langs:config_lsp()
-    langs:config_mason()
-    langs:config_treesitter()
-    langs:config_formatter()
-  end,
+  end
+end
 
-  solve = function(langs, mod)
-    local mt = { __index = mod }
-    for _, lang in ipairs(mod) do
-      if type(lang) == "string" then
-        langs:append(lang, mod)
-      elseif type(lang) == "table" then
-        langs:solve(setmetatable(lang, mt))
-      end
-    end
-  end,
-
-  ---@type config.language.Collect
-  collect = function(langs, callback)
-    vim.wait(1000, function() return langs.ok end)
-    return mapfold({}, callback, langs.get)
-  end,
-}
-
-local metatable = {
-  __index = function(self, key)
-    local result = properties[key]
-    return result and self:collect(result) or lang_functions[key]
-  end,
-}
-
----@type {new: fun():config.language.Langs; config:fun(config.language.Langs)}
-return {
-  new = function()
-    return setmetatable({
-      get = {},
-    }, metatable)
-  end,
-}
+return Langs
