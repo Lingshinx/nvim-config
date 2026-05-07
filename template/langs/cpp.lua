42e446e0f29f05cd4416a8c1da91a47e30ca75f2
lingshin <yokaringx@outlook.com>
2026-05-07 15:31:30 +0800

    update(langs): use lz.n spec for plugins

--- template/langs/cpp.lua
+++ template/langs/cpp.lua
@@ -1,5 +1,5 @@
 return {
   formatter = "clang-format",
-  plugins = { "p00f/clangd_extensions.nvim", lazy = true },
+  plugins = { "p00f/clangd_extensions.nvim", ft = { "c", "cpp" } },
   lsp = "clangd",
 }
