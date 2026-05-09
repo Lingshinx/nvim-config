730c510ce97c3a30af9c5197f1e6a737b760b710
lingshin <yokaringx@outlook.com>
2026-05-09 18:17:59 +0800

    fix(utils.treesitter): ensure install after plugins loaded

--- lua/utils/plugin/treesitter.lua
+++ lua/utils/plugin/treesitter.lua
@@ -1,6 +1,8 @@
+local after = require("utils.pack").after_wrap
+
 return {
   ---@param langs string[]
-  ensure_installed = function(langs)
+  ensure_installed = after("nvim-treesitter", function(langs)
     local ts = require "nvim-treesitter"
 
     local installed = {}
@@ -10,5 +12,5 @@ return {
 
     local not_installed = vim.iter(langs):filter(function(lang) return not installed[lang] end):totable()
     ts.install(not_installed)
-  end,
+  end),
 }
