54457a6dc4968ca6804c206753631878a918b1b5
lingshin <yokaringx@outlook.com>
2026-05-12 21:32:01 +0800

    feat(utils.update): use utils.command to create command

--- plugin/update.lua
+++ plugin/update.lua
@@ -1,4 +1,16 @@
-vim.api.nvim_create_user_command("Update", function(opts) require("utils.update").update(opts.args) end, {
-  nargs = 1,
-  complete = function() return vim.tbl_keys(require("utils.update").updaters) end,
+require("utils.command").create("Update", {
+  treesitter = {
+    cmd = function(args, opts)
+      if opts.bang then require("utils.update").clean.treesitter() end
+      require("utils.update").update.treesitter(args)
+    end,
+    complete = function(arg_lead)
+      if not package.loaded["nvim-treesitter"] then require("lz.n").trigger_load "nvim-treesitter" end
+      local installed = require("nvim-treesitter").get_installed()
+      if arg_lead:sub(-1) == " " then return installed end
+      local args = vim.split(arg_lead, "%s+")
+      local last_args = args[#args]
+      return vim.iter(installed):filter(function(parser) return parser:find("^" .. last_args) end):totable()
+    end,
+  },
 })
