b03816293f37ac4ef665e1d99eee21d9eb8c1da5
lingshin <yokaringx@outlook.com>
2026-05-13 16:50:41 +0800

    refactor(utils.update): rename upadte.lua to manage.lua and refactor the struct

---
+++ lua/utils/manager.lua
@@ -0,0 +1,49 @@
+---@class config.manager.Spec
+---@field before? fun(self: table)
+---@field update fun(items: string[], self: table)
+---@field clean? fun(self: table)
+---@field complete? fun(self: table)
+
+local function load_treesitter()
+  if not package.loaded["nvim-treesitter"] then require("lz.n").trigger_load "nvim-treesitter" end
+  return require "nvim-treesitter"
+end
+
+local managers = {
+  treesitter = {
+    before = function(self) self.treesitter = load_treesitter() end,
+    update = function(languages, self) self.treesitter.update(languages) end,
+    clean = function(self)
+      local treesitter = self.treesitter
+      local neededs = require("utils.fn").contains_map(require("load.langs").treesitter)
+      local unneededs = vim
+        .iter(treesitter().get_installed())
+        :filter(function(parser) return not neededs[parser] end)
+        :totable()
+      treesitter.uninstall(unneededs)
+    end,
+    complete = function(self) return self.treesitter.get_installed() end,
+  },
+
+  plugin = {
+    update = function(names)
+      if not names or vim.tbl_isempty(names) then
+        names = vim.iter(vim.pack.get()):map(function(pack) return pack.spec.name end):totable()
+      end
+      vim.pack.update(names)
+    end,
+    clean = function()
+      local inactives = vim
+        .iter(vim.pack.get())
+        :filter(function(pack) return not pack.active end)
+        :map(function(pack) return pack.spec.name end)
+        :totable()
+      vim.pack.del(inactives)
+    end,
+    complete = function()
+      return vim.iter(vim.pack.get()):map(function(pack) return pack.spec.name end):totable()
+    end,
+  },
+}
+
+return managers
