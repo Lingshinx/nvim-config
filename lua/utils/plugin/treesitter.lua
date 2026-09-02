df6cf220289fc9491c811f58e0bf40a47b7e7634
lingshin <yokaringx@outlook.com>
2026-09-02 18:34:17 +0800

    refactor(utils.treesitter): use a little func to check installed

--- lua/utils/plugin/treesitter.lua
+++ lua/utils/plugin/treesitter.lua
@@ -1,15 +1,11 @@
 local after = require("utils.pack").after_wrap
+local contains_map = require("utils.fn").contains_map
 
 return {
   ---@param langs string[]
   ensure_installed = after("nvim-treesitter", function(langs)
     local ts = require "nvim-treesitter"
-
-    local installed = {}
-    for lang in ipairs(ts.get_installed()) do
-      installed[lang] = true
-    end
-
+    local installed = contains_map(ts.get_installed())
     local not_installed = vim.iter(langs):filter(function(lang) return not installed[lang] end):totable()
     ts.install(not_installed)
   end),
